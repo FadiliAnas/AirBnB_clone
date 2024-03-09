4347f33f0c03a4ff705c5d320c0920b386b858d9
Amineafilal <amineafilalelalami@gmail.com>
2024-03-09 12:42:37 -0800

    all test

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
 
     def save(self):
         """ This method represent updates the public instance attribute """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
         models.storage.save()
 
     def to_dict(self):
