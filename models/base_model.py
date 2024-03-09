6f4b90397748d5f46e1a005854bbff5ac4ed7604
Amineafilal <amineafilalelalami@gmail.com>
2024-03-09 13:04:07 -0800

    all test

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
-            self.updated_at = self.created_at()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
