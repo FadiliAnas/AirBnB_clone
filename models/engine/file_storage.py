4ae8be9232470e6d9fccecd839f28c355d78f370
Amineafilal <amineafilalelalami@gmail.com>
2024-03-10 21:07:33 +0000

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-""" This module defines storage """
-import json
+import unittest
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -8,36 +7,54 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from models import storage
 
 
-class FileStorage:
-    """This class for file storage """
-    __file_path = "file.json"
-    __objects = {}
+class TestFileStorage(unittest.TestCase):
+    def setUp(self):
+        """Set up for testing"""
+        self.test_storage = storage.FileStorage()
 
-    def all(self):
-        """This function represents all"""
-        return self.__objects
-
-    def new(self, obj):
-        """This function represents new"""
-        keyequal = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[keyequal] = obj
-
-    def save(self):
-        """This function represents save"""
-        json_dict = {}
-        for k, v in FileStorage.__objects.items():
-            json_dict[k] = v.to_dict()
-        with open(FileStorage.__file_path, 'w') as filejson:
-            json.dump(json_dict, filejson, indent=4)
-
-    def reload(self):
-        """This function represents reload"""
+    def tearDown(self):
+        """Tear down after testing"""
         try:
-            with open(FileStorage.__file_path, 'r') as filejsonload:
-                dict_load = json.load(filejsonload)
-                for k, v in dict_load.items():
-                    FileStorage.__objects[k] = eval(v["__class__"])(**v)
+            os.remove(storage.FileStorage._FileStorage__file_path)
         except FileNotFoundError:
-            return
+            pass
+
+    def test_all(self):
+        """Test all method"""
+        all_objs = self.test_storage.all()
+        self.assertEqual(type(all_objs), dict)
+        self.assertIs(all_objs, self.test_storage._FileStorage__objects)
+
+    def test_new(self):
+        """Test new method"""
+        new_user = User()
+        self.test_storage.new(new_user)
+        key = "{}.{}".format(new_user.__class__.__name__, new_user.id)
+        self.assertIn(key, self.test_storage._FileStorage__objects)
+
+    def test_save(self):
+        """Test save method"""
+        new_user = User()
+        self.test_storage.new(new_user)
+        self.test_storage.save()
+        with open(storage.FileStorage._FileStorage__file_path, 'r') as file:
+            data = file.read()
+            self.assertIn(new_user.__class__.__name__, data)
+            self.assertIn(new_user.id, data)
+
+    def test_reload(self):
+        """Test reload method"""
+        new_user = User()
+        self.test_storage.new(new_user)
+        self.test_storage.save()
+        self.test_storage.reload()
+        key = "{}.{}".format(new_user.__class__.__name__, new_user.id)
+        self.assertIn(key, self.test_storage._FileStorage__objects)
+        self.assertEqual(self.test_storage._FileStorage__objects[key].to_dict(), new_user.to_dict())
+
+
+if __name__ == '__main__':
+    unittest.main()
