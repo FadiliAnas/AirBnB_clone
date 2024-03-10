14317560e5a7ad8efe6989aa40b363dab189b4b5
Amineafilal <amineafilalelalami@gmail.com>
2024-03-10 07:26:58 -0700

    unittest

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -24,31 +24,31 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsNotNone(obj.updated_at)
         mock_storage.new.assert_called_once_with(obj)
 
-        def test_str(self):
-            obj = BaseModel()
-            string_representation = str(obj)
-            self.assertTrue('[BaseModel]' in string_representation)
-            self.assertTrue(obj.id in string_representation)
-            self.assertTrue(obj.to_dict()['id'] in string_representation)
+    def test_str(self):
+        obj = BaseModel()
+        string_representation = str(obj)
+        self.assertTrue('[BaseModel]' in string_representation)
+        self.assertTrue(obj.id in string_representation)
+        self.assertTrue(obj.to_dict()['id'] in string_representation)
 
-        @patch('models.storage')
-        def test_save(self, mock_storage):
-            obj = BaseModel()
-            original_updated_at = obj.updated_at
-            obj.save()
-            self.assertNotEqual(obj.updated_at, original_updated_at)
-            mock_storage.save.assert_called_once()
+    @patch('models.storage')
+    def test_save(self, mock_storage):
+        obj = BaseModel()
+        original_updated_at = obj.updated_at
+        obj.save()
+        self.assertNotEqual(obj.updated_at, original_updated_at)
+        mock_storage.save.assert_called_once()
 
-        def test_to_dict(self):
-            obj = BaseModel()
-            obj_dict = obj.to_dict()
-            self.assertIsInstance(obj_dict, dict)
-            self.assertEqual(obj_dict['id'], obj.id)
-            self.assertEqual(obj_dict['__class__'], 'BaseModel')
-            self.assertEqual(obj_dict['created_at'], \
-                             obj.created_at.isoformat())
-            self.assertEqual(obj_dict['updated_at'], \
-                             obj.updated_at.isoformat())
+    def test_to_dict(self):
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['id'], obj.id)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['created_at'], \
+                         obj.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], \
+                         obj.updated_at.isoformat())
 
 
 if __name__ == '__main__':
