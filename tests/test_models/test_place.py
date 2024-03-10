5126fa557d21481e5e9664e61800fe9309e37b3d
Amineafilal <amineafilalelalami@gmail.com>
2024-03-10 21:25:43 +0000

    test

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,45 +2,35 @@
 
 import unittest
 from models.place import Place
-from models.amenity import Amenity
-from models.city import City
-from models.user import User
 
+class TestPlace(unittest.TestCase):
+    def test_attributes(self):
+        place = Place()
+        self.assertTrue(hasattr(place, 'city_id'))
+        self.assertTrue(hasattr(place, 'user_id'))
+        self.assertTrue(hasattr(place, 'name'))
+        self.assertTrue(hasattr(place, 'description'))
+        self.assertTrue(hasattr(place, 'number_rooms'))
+        self.assertTrue(hasattr(place, 'number_bathrooms'))
+        self.assertTrue(hasattr(place, 'max_guest'))
+        self.assertTrue(hasattr(place, 'price_by_night'))
+        self.assertTrue(hasattr(place, 'latitude'))
+        self.assertTrue(hasattr(place, 'longitude'))
+        self.assertTrue(hasattr(place, 'amenity_ids'))
 
-class TestAmenity(unittest.TestCase):
-    """Unittest for amenity"""
-
-    def test_save(self):
-        """test save"""
-        amenity = Amenity()
-        amenity.save()
-        self.assertNotEqual(amenity.created_at, amenity.updated_at)
-
-    def test_str(self):
-        """test str"""
-        amenity = Amenity()
-        amenity.save()
-        self.assertEqual(str(amenity), "[Amenity] ({}) {}".format(
-            amenity.id, amenity.__dict__))
-
-    def test_type_amenity(self):
-        """test type amenity"""
-        self.new_amenity = Amenity()
-        self.assertEqual(self.new_amenity.name, "")
-
-    def test_name(self):
-        """Test name"""
-        amenity = Amenity()
-        amenity.name = "Wifi"
-        self.assertEqual(amenity.name, "Wifi")
-
-    def test_id(self):
-        """Test id"""
-        amenity = Amenity()
-        amenity.name = "Wifi"
-        self.assertIsInstance(amenity.id, str)
-        self.assertEqual(len(amenity.id), 36)
-
+    def test_default_values(self):
+        place = Place()
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
+        self.assertEqual(place.amenity_ids, [])
 
 if __name__ == '__main__':
     unittest.main()
