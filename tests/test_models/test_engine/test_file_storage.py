c69d2bc8663bb777ff503aa3c37ee4219b5186f2
Amineafilal <amineafilalelalami@gmail.com>
2024-03-10 21:35:24 +0000

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,46 +3,36 @@
 Test of file Storage
 """
 import unittest
-from unittest.mock import patch
+import json
 import os
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 from models.engine.file_storage import FileStorage
-import json
+import models
+
 
 class TestFileStorage(unittest.TestCase):
+    """Unit tests for FileStorage"""
 
     def setUp(self):
-        self.file_path = "test_file.json"
+        """Set up the test cases"""
         self.storage = FileStorage()
-        self.storage._FileStorage__file_path = self.file_path
+        setattr(self.storage, "_FileStorage__objects", {})
+        self.file_path = models.storage._FileStorage__file_path
+        self.instance = BaseModel()
+        self.objs = models.storage._FileStorage__objects
+        self.keyname = f"BaseModel.{self.instance.id}"
 
-    def tearDown(self):
-        if os.path.exists(self.file_path):
-            os.remove(self.file_path)
-    def save(self):
-        json_dict = {}
-        for k, v in FileStorage.__objects.items():
-            json_dict[k] = v.to_dict()
-        print("File path:", FileStorage.__file_path)
-        with open(FileStorage.__file_path, 'w') as filejson:
-            json.dump(json_dict, filejson, indent=4)
+    def test_all_method_exists(self):
+        """Test if the 'all' method exists"""
+        self.assertTrue(hasattr(models.storage, "all"))
 
-    def test_all(self):
-        obj = BaseModel()
-        obj_dict = obj.to_dict()
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.storage.new(obj)
-        all_objs = self.storage.all()
-        self.assertIn(key, all_objs)
-        self.assertEqual(all_objs[key], obj)
+    def test_new_method_exists(self):
+        """Test if the 'new' method exists"""
+        self.assertTrue(hasattr(models.storage, "new"))
 
-   
+        expected_data = {key: value.to_dict() for key,
+                         value in self.objs.items()}
+        self.assertEqual(saved_data, expected_data)
 
 
 if __name__ == "__main__":
