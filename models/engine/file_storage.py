091d6182909f6d60f3d3585f0c71487188d2e843
FadiliAnas <anasfadili65@gmail.com>
2024-03-10 11:54:48 +0000

    pycodestyle fixing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,29 +9,34 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-class  FileStorage:
+
+class FileStorage:
     __file_path = "file.json"
     __objects = {}
+
     def all(self):
         """This function represents all"""
         return self.__objects
+
     def new(self, obj):
         """This function represents new"""
         keyequal = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[keyequal] = obj
+
     def save(self):
         """This function represents save"""
         json_dict = {}
-        for k,v in FileStorage.__objects.items():
+        for k, v in FileStorage.__objects.items():
             json_dict[k] = v.to_dict()
-        with open(FileStorage.__file_path, 'w') as filejson: 
+        with open(FileStorage.__file_path, 'w') as filejson:
             json.dump(json_dict, filejson, indent=4)
+
     def reload(self):
         """This function represents reload"""
         try:
             with open(FileStorage.__file_path, 'r') as filejsonload:
                 dict_load = json.load(filejsonload)
-                for k,v in dict_load.items():
+                for k, v in dict_load.items():
                     FileStorage.__objects[k] = eval(v["__class__"])(**v)
-        except FileNotFoundError :
+        except FileNotFoundError:
             return
