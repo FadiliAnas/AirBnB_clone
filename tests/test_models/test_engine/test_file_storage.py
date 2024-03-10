9f5bb486762e1d45cbdf2294083b1cf2846e5a88
Amineafilal <amineafilalelalami@gmail.com>
2024-03-10 23:39:38 +0000

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -32,36 +32,34 @@ class TestFileStorage(unittest.TestCase):
         self.assertTrue(hasattr(models.storage, "all"))
 
     def test_new_method_exists(self):
-        """Test new method"""
+        """Test new method exists"""
         self.assertTrue(hasattr(models.storage, "new"))
 
     def test_reload_method_exists(self):
-        """Test reload method"""
+        """Test reload method exists"""
         self.assertTrue(hasattr(models.storage, "reload"))
 
     def test_all_method_returns_dict(self):
-        """Test all method"""
+        """Test all method return dict"""
         result = models.storage.all()
         self.assertIsInstance(result, dict)
 
     def test_new_method_adds_to_objects(self):
-        """Test new method add method"""
+        """Test new method add to objects"""
         obj = BaseModel()
         models.storage.new(obj)
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.assertIn(key, models.storage.all().keys())
 
     def test_save_method_saves_to_file(self):
-        """Test if the 'save' method saves to the file"""
+        """Test save method saves to file"""
         my_model = BaseModel()
         my_model.name = "My_First_Model"
         my_model.my_number = 89
         models.storage.new(my_model)
         models.storage.save()
-
         with open(self.file_path, "r") as data_file:
             saved_data = json.load(data_file)
-
         expected_data = {key: value.to_dict() for key,
                          value in self.objs.items()}
         self.assertEqual(saved_data, expected_data)
