22c302b1051c67b4a1ba01fca5967916b0a4a2b7
Amineafilal <amineafilalelalami@gmail.com>
2024-03-10 22:16:16 +0000

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -14,14 +14,11 @@ class TestFileStorage(unittest.TestCase):
     def setUp(self):
         """Set up the test cases"""
         self.storage = FileStorage()
-        setattr(FileStorage, "_FileStorage__objects", {})
-    
-    def tearDown(self):
-        """Clean up"""
-        try:
-            os.remove(FileStorage._FileStorage__file_path)
-        except FileNotFoundError:
-            pass
+        setattr(self.storage, "_FileStorage__objects", {})
+        self.file_path = models.storage._FileStorage__file_path
+        self.instance = BaseModel()
+        self.objs = models.storage._FileStorage__objects
+        self.keyname = f"BaseModel.{self.instance.id}"
 
     def test_all_method_exists(self):
         """Test if the 'all' method exists"""
@@ -47,6 +44,21 @@ class TestFileStorage(unittest.TestCase):
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.assertIn(key, models.storage.all().keys())
 
-   
+    def test_save_method_saves_to_file(self):
+        """Test if the 'save' method saves to the file"""
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+        models.storage.new(my_model)
+        models.storage.save()
+
+        with open(self.file_path, "r") as data_file:
+            saved_data = json.load(data_file)
+
+        expected_data = {key: value.to_dict() for key,
+                         value in self.objs.items()}
+        self.assertEqual(saved_data, expected_data)
+
+
 if __name__ == "__main__":
     unittest.main()
